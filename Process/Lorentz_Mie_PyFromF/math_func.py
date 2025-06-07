cda45510613618af7cceb71760daf02b4557b7b0
Frank-Deng-WH6HS <enter_the_dragon@foxmail.com>
2025-06-07 16:50:20 +0800

    完成对Fortran代码中power和f函数的改写. (2025-06-07)

--- Process/Lorentz_Mie_PyFromF/math_func.py
+++ Process/Lorentz_Mie_PyFromF/math_func.py
@@ -4,22 +4,24 @@
 #[1]上海计算技术研究所. 电子计算机算法手册[M]. 上海: 上海教育出版社, 1982. 
 #[2]喻文健. 数值分析与算法[M]. 3版. 北京: 清华大学出版社, 2020. 
 
-import math; 
+from . import np; 
+from numpy import float64 as fp64; 
 
 #获取当前系统浮点数的机器精度
 def machine_precision(): 
-    eps = 1e0; tol1 = math.inf; 
-    while tol1 > 1e0: 
-        eps *= 0.5e0; 
-        tol1 = 1e0 + eps; 
+    eps = fp64(1e0); tol1 = np.inf; 
+    while tol1 > fp64(1e0): 
+        eps *= fp64(0.5e0); 
+        tol1 = fp64(1e0) + eps; 
     return eps; 
 
 #求一元函数零点的ZeroIn算法(结合了二分法, 线性插值和逆二次插值[1-2])
-def zeroin(a_x: float, b_x: float, f: callable, tol: float) -> float: 
+def zeroin(a_x: np.float64, b_x: np.float64, 
+    f: callable, tol: np.float64) -> np.float64: 
     #Label 10: 获取浮点数的机器精度
     eps = machine_precision(); 
     #Label 15: 初始化
-    a = a_x; b = b_x; 
+    a = fp64(a_x); b = fp64(b_x); 
     f_a = f(a); f_b = f(b); 
     tail_loop_lbl20 = True; 
     while True: 
@@ -32,8 +34,8 @@ def zeroin(a_x: float, b_x: float, f: callable, tol: float) -> float:
             a = b; b = c; c = a; 
             f_a = f_b; f_b = f_c; f_c = f_a; 
         #Label 40: 计算过程中实际使用的精度
-        tol1 = 2e0 * eps * abs(b) + 0.5e0 * tol; 
-        xm = 0.5e0 * (c - b); 
+        tol1 = fp64(2e0) * eps * abs(b) + fp64(0.5e0) * tol; 
+        xm = fp64(0.5e0) * (c - b); 
         #Label 44: 跳出循环的条件
         if abs(xm) <= tol1 or f_b == 0e0: 
             break; 
@@ -47,20 +49,21 @@ def zeroin(a_x: float, b_x: float, f: callable, tol: float) -> float:
                 #Label 50: 
                 q = f_a / f_c; r = f_b / f_c; 
                 s = f_b / f_a; 
-                p = s * (2e0 * xm * q * (q - r) - (b - a) * (r - 1e0)); 
-                q = (q - 1e0) * (r - 1e0) * (s - 1e0); 
+                p = s * (fp64(2e0) * xm * q * (q - r) \
+                    - (b - a) * (r - fp64(1e0))); 
+                q = (q - fp64(1e0)) * (r - 1e0) * (s - 1e0); 
             else: 
                 #Label 48: 
                 s = f_b / f_a; 
-                p = 2e0 * xm * s; 
-                q = 1e0 - s; 
+                p = fp64(2e0) * xm * s; 
+                q = fp64(1e0) - s; 
             #Label 60: 
-            if p > 0: 
+            if p > fp64(0e0): 
                 q = -q; 
             p = abs(p); 
             #Label 64: 
-            if (2e0 * p >= 3e0 * xm * q - abs(tol1 * q)) or \
-                (p >= abs(0.5e0 * e * q)): 
+            if (fp64(2e0) * p >= fp64(3e0) * xm * q - abs(tol1 * q)) \
+                or (p >= abs(fp64(0.5e0) * e * q)): 
                 #Label 70: 
                 d = xm; e = d; 
             else: 
@@ -71,8 +74,31 @@ def zeroin(a_x: float, b_x: float, f: callable, tol: float) -> float:
         if abs(d) > tol1: 
             b += d; 
         else: 
-            b += math.copysign(tol1, xm); 
+            b += np.copysign(tol1, xm); 
         f_b = f(b); 
         #Label 85: 
-        tail_loop_lbl20 = bool(f_b * (f_c / abs(f_c)) > 0e0); 
+        tail_loop_lbl20 = bool(f_b * (f_c / abs(f_c)) > fp64(0e0)); 
     return b; 
+
+#F函数
+#在源代码中是被POWER(A, B, R1, R2)调用的一元函数(与ZEROIN合用)
+#由于python传参方式的特殊性, 其用法改为r = f_trivar(a, b, r1)
+#在后续的power函数中, f_trivar将通过lambda表达式重新构造一元函数f
+def f_trivar(a: np.float64, b: np.float64, 
+    r_1: np.float64) -> np.float64: 
+    r_2 = (fp64(1e0) + b) * fp64(2e0) * a - r_1; 
+    f = (r_2 - r_1) / np.log(r_2 / r_1) - a; 
+    return f; 
+
+#Power函数
+#由于python传参方式的特殊性, 其用法改为(r_1, r_2) = power(a, b, r_1, r_2)
+#返回的结果是包含两个np.float64的tuple
+def power(a: np.float64, b: np.float64, 
+    r_1: np.float64, r_2: np.float64) -> tuple: 
+    '''Usage: (r_1, r_2) = power(a, b, r_1, r_2)'''
+    f = lambda r_1: f_trivar(a, b, r_1); 
+    a_x = fp64(1e-12); b_x = a; 
+    r_min = zeroin(a_x, b_x, f, fp64(0e0)); 
+    r_max = (fp64(1e0) + b) * 2 * a - r_min; 
+    return (r_min, r_max); 
+
