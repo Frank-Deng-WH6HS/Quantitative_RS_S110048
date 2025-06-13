312754aa6a41458c9da0ac710daf42f06b249a9e
Frank-Deng-WH6HS <enter_the_dragon@foxmail.com>
2025-06-13 15:57:43 +0800

    在zeroin, f, power和gauss函数中启用传入参数类型检查, 基于inspect库. (2025-06-13)

--- Process/Lorentz_Mie_PyFromF/math_func.py
+++ Process/Lorentz_Mie_PyFromF/math_func.py
@@ -4,7 +4,13 @@
 #[1]上海计算技术研究所. 电子计算机算法手册[M]. 上海: 上海教育出版社, 1982. 
 #[2]喻文健. 数值分析与算法[M]. 3版. 北京: 清华大学出版社, 2020. 
 
+#注意: 本模块内所有需要传参的函数, 均需执行传入参数类型检查. 
+#当传入的实参类型与函数声明的形参类型不符时, 将抛出TypeError异常. 
+
+import inspect; 
+
 from . import np; 
+import pyfunc_type; 
 from numpy import float64 as fp64; 
 
 #获取当前系统浮点数的机器精度
@@ -19,10 +25,11 @@ def machine_precision():
 def zeroin(a_x: np.float64, b_x: np.float64, 
     f: callable, tol: np.float64) -> np.float64: 
     '''Usage: x = zeroin(a_x, b_x, f, tol)'''
+    pyfunc_type.type_check(); 
     #Label 10: 获取浮点数的机器精度
     eps = machine_precision(); 
     #Label 15: 初始化
-    a = fp64(a_x); b = fp64(b_x); 
+    a = a_x; b = b_x; 
     f_a = f(a); f_b = f(b); 
     tail_loop_lbl20 = True; 
     while True: 
@@ -88,6 +95,7 @@ def zeroin(a_x: np.float64, b_x: np.float64,
 def f_trivar(a: np.float64, b: np.float64, 
     r_1: np.float64) -> np.float64: 
     '''Usage: r = f_trivar(a, b, r_1)'''
+    pyfunc_type.type_check(); 
     r_2 = (fp64(1e0) + b) * fp64(2e0) * a - r_1; 
     f = (r_2 - r_1) / np.log(r_2 / r_1) - a; 
     return f; 
@@ -98,6 +106,7 @@ def f_trivar(a: np.float64, b: np.float64,
 def power(a: np.float64, b: np.float64, 
     r_1: np.float64, r_2: np.float64) -> tuple: 
     '''Usage: (r_1, r_2) = power(a, b, r_1, r_2)'''
+    pyfunc_type.type_check(); 
     f = lambda r_1: f_trivar(a, b, r_1); 
     a_x = fp64(1e-12); b_x = a; 
     r_min = zeroin(a_x, b_x, f, fp64(0e0)); 
@@ -105,17 +114,13 @@ def power(a: np.float64, b: np.float64,
     return (r_min, r_max); 
 
 #Power函数
-#由于python传参方式的特殊性, 此处强制要求对z和w执行类型检查
+#由于python传参方式的特殊性, 此处强制要求对z和w执行类型检查, 要求
+#二者均须为可变(mutable)类型对象np.ndarray, 方可通过传引用方式修改. 
 #返回的结果为None
 def gauss(n: int, ind_1: int, ind_2: int, 
     z: np.ndarray, w: np.ndarray) -> tuple: 
     '''Usage: gauss(n, ind_1, ind_2, z, w), where z and w will be modified. '''
-    #输入参数类型检查
-    if type(z) is not np.ndarray: 
-        raise TypeError("z ")
-    if type(w) is not np.ndarray: 
-        raise 
-    #正式计算
+    pyfunc_type.type_check(); 
     a = fp64(1e0); b = fp64(2e0); c = fp64(3e0); 
     ind = n % 2; k = n // 2 + ind; f = fp64(n); 
     for i in range(1, k + 1): 
@@ -123,11 +128,11 @@ def gauss(n: int, ind_1: int, ind_2: int,
         if i == 1: 
             x = a - b /((f + a) * f); 
         if i == 2: 
-            x = (z_[n] -a ) * fp64(4) + z_[n]
+            x = (z[n] - a) * fp64(4) + z[n]
         if i == 3: 
-            x = (z_[n - 1] - z_[n]) * fp64(1.6e0) + z_[n - 1]; 
+            x = (z[n - 1] - z[n]) * fp64(1.6e0) + z[n - 1]; 
         if i > 3: 
-            x = (z_[m + 1] - z_[m + 2]) * c + z_[m + 3]; 
+            x = (z[m + 1] - z[m + 2]) * c + z[m + 3]; 
         if i == k and ind == 1: 
             x = fp64(0e0); 
         n_iter = 0; 
@@ -152,13 +157,13 @@ def gauss(n: int, ind_1: int, ind_2: int,
             first_loop_lbl10 = False; 
         z[m] = x; w[m] = pa ** 2 * (a - x ** 2); 
         if ind_1 == 0: 
-            w_[m] *= b; 
+            w[m] *= b; 
         if i == k and ind == 1: 
             continue; 
-        z_[i] = -z_[m]; w_[i] = w_[m]; 
+        z[i] = -z[m]; w[i] = w[m]; 
     #Label 100: 
     pass; #原本这里是if-else, 每个分支都有一个print
     #Label 115: 
     if not (ind_1 == 0): 
-        z_ += a; z_ /= b; 
-    return (z_, w_); 
+        z += a; z /= b; 
+
